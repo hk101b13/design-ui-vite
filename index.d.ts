0b5e83955902b97c18a9c59bb6af766e713750d7
weizhaoma <weizhao.ma@qsan.com>
2023-12-18 15:03:48 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,64 @@
-import { TableProps, SwitchProps } from "antd";
-import { CustomTableProps } from "./src/Components/QTable";
-import { CustomSwitchProps } from "./src/Components/QSwitch";
-import { QButtonProps } from "./src/Components/QButton";
-import { QThemeProviderProps } from "./src/Components/QThemeProvider";
+declare module "design-ui-vite" {
+  import { TableProps } from "antd";
+  import styled, { Interpolation } from "@emotion/styled";
+  import { Theme } from "@emotion/react";
+  import { SwitchProps } from "antd";
+  import { ReactNode } from "react";
 
-declare const QTable: (
-  props: CustomTableProps & TableProps<any>
-) => JSX.Element;
+  export interface TableColor {
+    headerColor?: string;
+    rowColor?: string;
+    hoverRowColor?: string;
+    textColor?: string;
+  }
 
-declare const QSwitch: (props: SwitchProps & CustomSwitchProps) => JSX.Element;
+  export interface CustomTableProps {
+    customStyle?: Interpolation<Theme>;
+    tableColor?: TableColor;
+  }
 
-declare const QThemeProvider: (props: QThemeProviderProps) => JSX.Element;
+  export interface CustomSwitchProps {
+    customStyle?: Interpolation<Theme>;
+  }
 
-declare const QButton: React.ForwardRefExoticComponent<
-  QButtonProps & React.RefAttributes<any>
->;
+  export interface QButtonProps {
+    onClick?: React.MouseEventHandler<HTMLElement> | undefined;
+    qType?: string;
+    qSize?: string;
 
-export { QTable, QSwitch, QThemeProvider, QButton };
+    disabled?: boolean;
+    loading?: boolean;
+    children?: ReactNode;
+    className?: string;
+    style?: React.CSSProperties;
+    customStyle?: Interpolation<Theme>;
+  }
+
+  export interface QThemeProviderProps {
+    colorScheme?: {
+      text?: string;
+      main?: string;
+      secondary?: string;
+      accent?: string;
+    };
+    fontSize?: {
+      t1?: number;
+      t2?: number;
+      t3?: number;
+      t4?: number;
+      t5?: number;
+    };
+    size?: "small" | "middle" | "large";
+    locale?: string;
+    localeBundle?: { lng: string; ns: "translation"; resources: any }[];
+    children: any;
+    fontFamily?: string;
+  }
+
+  const QTable: React.FC<CustomTableProps & TableProps<any>>;
+  const QSwitch: React.FC<CustomSwitchProps & SwitchProps>;
+  const QButton: React.FC<QButtonProps>;
+  const QThemeProvider: React.FC<QThemeProviderProps>;
+
+  export { QTable, QButton, QSwitch, QThemeProvider };
+}
